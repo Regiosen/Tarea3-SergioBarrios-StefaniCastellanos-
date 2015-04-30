5ab18913d0e7b0947b7ebd1b96b4da2343446084
Regiosen <Alenios@hotmail.com>
2015-04-29 20:57:07 -0430

    Primer Commit

---
+++ Tarea3(SergioBarrios-StefanyCastellanos)/BilleteraElectronica.py
@@ -0,0 +1,17 @@
+'''
+Created on 29/4/2015
+
+@author: Sergio Luis Barrios
+'''
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
