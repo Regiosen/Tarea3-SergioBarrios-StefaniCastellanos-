0dccbf068b0e8747e3de326304b5d310a8ab7a22
Regiosen <Alenios@hotmail.com>
2015-05-06 21:00:31 -0430

    Implementacion de verificacion de PIN

--- Tarea3(SergioBarrios-StefanyCastellanos)/BilleteraElectronica.py
+++ Tarea3(SergioBarrios-StefanyCastellanos)/BilleteraElectronica.py
@@ -1,55 +1,73 @@
+# -*- coding: UTF-8 -*-
 '''
 Created on 29/4/2015
-
 @author: Sergio Luis Barrios
          Stefani Castellanos
+         Francisco Sucre
 '''
 
-import sys
+from decimal import Decimal
+from datetime import datetime
+import hashlib
 
 class Creditos(object):
-    def __init__(self,mont,fecha,estab):
-        self.monto = mont
-        self.fecha_transaccion = fecha
-        self.id_establecimiento = estab
+    
+    def __init__(self, monto, id_establecimiento):
+        
+        if (isinstance(monto, str) or isinstance(monto, bool) ):
+            raise Exception("Debe ingresar un monto numerico")
+        
+        self.monto = Decimal(monto).quantize(Decimal('1.00'))
+        self.fecha_transaccion = datetime.today()
+        self.id_establecimiento = id_establecimiento
 
 class Debitos(object):
-    def __init__(self,mont,fecha,estab):
-        self.monto = mont    
-        self.fecha_transaccion = fecha
-        self.id_establecimiento = estab         
-
+    def __init__(self, monto, id_establecimiento):
+        
+        if (isinstance(monto, str) or isinstance(monto, bool) ):
+            raise Exception("Debe ingresar un monto numerico de tipo Decimal")
+        
+        self.monto = Decimal(monto).quantize(Decimal('1.00'))
+        self.fecha_transaccion = datetime.today()
+        self.id_establecimiento = id_establecimiento
+        
 class BilleteraElectronica(object):
-    '''
-    classdocs
-    '''
 
-    def __init__(self, Identificador,Nombre,Apellido, Cedula, PIN):
-        self.ID = Identificador
-        self.nombre = Nombre
-        self.apellido = Apellido
-        self.ci= Cedula
-        self.pin = PIN
+    def __init__(self, ID, nombre, apellido, CI, PIN):
+        
+        if (not isinstance(CI, int) or (CI <= 0)):
+            raise Exception("Debe ingresar un monto numerico")
+        
+        self.ID = ID 
+        self.nombre = nombre
+        self.apellido = apellido
+        self.CI = CI 
+        self.PIN = hashlib.sha512(PIN.encode()).hexdigest()
         self.creditos = []
         self.debitos = []
         self.saldo = 0
         
     def Saldo(self):
-        self.saldo = 0
-        for i in self.creditos:
-            self.saldo = self.saldo + i.monto 
-        for j in self.debitos:
-            self.saldo = self.saldo - j.monto
-        if (self.saldo <0):
-            sys.exit ('ERROR: Saldo negativo')
-        elif (self.saldo >=0):     
-            return self.saldo
+        return Decimal(self.saldo).quantize(Decimal('1.00'))
             
     def Recargar(self,creditoEntrante):
+        
+        if (creditoEntrante.monto <= 0):
+            raise Exception("No es posible recargar una cantidad no positiva")       
+
         self.creditos.append(creditoEntrante)
-        self.saldo = self.saldo + creditoEntrante.monto
+        self.saldo += creditoEntrante.monto
         
-    def Consumir(self,debitoEntrante):
+    def Consumir(self,debitoEntrante,PIN):
+        
+        if (debitoEntrante.monto <= 0):
+            raise Exception("No es posible consumir una cantidad no positiva")     
+
+        if (self.saldo - debitoEntrante.monto < 0):
+            raise Exception("No tiene sufieciente fondos para efectuar la operacion")  
+        
+        if (self.PIN != hashlib.sha512(PIN.encode()).hexdigest()):
+            raise Exception("El pin insertado es incorrecto")  
+        
         self.debitos.append(debitoEntrante)
-        self.saldo = self.saldo - debitoEntrante.monto
-        
+        self.saldo -= debitoEntrante.monto
--- Tarea3(SergioBarrios-StefanyCastellanos)/PruebasTarea3.py
+++ Tarea3(SergioBarrios-StefanyCastellanos)/PruebasTarea3.py
@@ -1,50 +1,145 @@
+# -*- coding: UTF-8 -*-
+'''
+Created on 29/4/2015
+@author: Sergio Luis Barrios
+         Stefani Castellanos
+         Francisco Sucre
+'''
+
 import unittest
 from BilleteraElectronica import *
+from decimal import Decimal
+from datetime import datetime
 
 class TestcalcularPrecio(unittest.TestCase):
 
-    def testUnCreditoNingunDebito(self):
-        billetera = BilleteraElectronica(1,'sergio','barrios',24101133,8)
-        lista = [Creditos(5,"25/4/2015","USB")]
-        billetera.creditos = lista
-        self.assertEqual(billetera.Saldo(),5,"No funciona calcular un solo credito")
+    def testUnCreditoCuentaVacia(self):
+        billetera = BilleteraElectronica(1,'sergio','barrios',24101133,"8")
+        cred = Creditos(5, "USB")
+        billetera.Recargar(cred)
+        self.assertEqual(billetera.Saldo(),5,"No funciona calcular un solo credito")       
         
     def testUnCreditoUnDebitoResultadoNoCero(self):
-        billetera = BilleteraElectronica(1,'sergio','barrios',24101133,8)
-        lista = [Debitos(5,"2/5/2015","USB")]
-        lista2 = [Creditos(7,"1/5/2015","USB")]
-        billetera.debitos = lista
-        billetera.creditos = lista2
+        billetera = BilleteraElectronica(1,'sergio','barrios',24101133,"8")
+        deb = Debitos(5, "USB")
+        cred = Creditos(7, "USB")
+        billetera.Recargar(cred)
+        billetera.Consumir(deb,"8")
         self.assertEqual(billetera.Saldo(),2,"No funciona calcular un credito y un debito")
         
     def testUnCreditoUnDebitoResultadoCero(self):
-        billetera = BilleteraElectronica(1,'sergio','barrios',24101133,8)
-        lista = [Debitos(2,"1/5/2015","USB")]
-        lista2 = [Creditos(2,"2/5/2015","USB")]
-        billetera.debitos = lista
-        billetera.creditos = lista2
+        billetera = BilleteraElectronica(1,'sergio','barrios',24101133, "8")
+        deb = Debitos(2, "USB")
+        cred = Creditos(2, "USB")
+        billetera.Recargar(cred)
+        billetera.Consumir(deb,"8")
         self.assertEqual(billetera.Saldo(),0,"No funciona calcular un credito y un debito que dejan en cero el saldo")
         
     def testRecargaUnica(self):
-        billetera = BilleteraElectronica(1,'sergio','barrios',24101133,8)
-        lista = [Debitos(2,"2/5/2015","USB")]
-        lista2 = [Creditos(2,"2/5/2015","USB")]
-        billetera.debitos = lista
-        billetera.creditos = lista2
-        recarga_nueva = Creditos(6,"2/5/2015","USB")
+        billetera = BilleteraElectronica(1,'sergio','barrios',24101133, "8")
+        deb = Debitos(2, "USB")
+        cred = Creditos(2, "USB")
+        billetera.Recargar(cred)
+        billetera.Consumir(deb,"8")
+        recarga_nueva = Creditos(6, "USB")
         billetera.Recargar(recarga_nueva)
         self.assertEqual(billetera.Saldo(),6,"No funciona recargar un credito")
         
+    def testConsumirConSaldoInsuficiente(self):
+        billetera = BilleteraElectronica(1,'francisco','sucre',19564959, "343")
+        deb = Debitos(2, "USB")
+        self.assertRaises(Exception,billetera.Consumir, deb )
+        
     def testConsumoUnicoConSuficienteCredito(self):
-        billetera = BilleteraElectronica(1,'sergio','barrios',24101133,8)
-        lista = [Debitos(2,"2/5/2015","USB")]
-        lista2 = [Creditos(9,"2/5/2015","USB")]
-        billetera.debitos = lista
-        billetera.creditos = lista2
-        consumo_nuevo = Debitos(4,"2/5/2015","USB")
-        billetera.Consumir(consumo_nuevo)
+        billetera = BilleteraElectronica(1,'sergio','barrios',24101133, "8")
+        deb = Debitos(2, "USB")
+        cred = Creditos(9, "USB")
+        billetera.Recargar(cred)
+        billetera.Consumir(deb,"8")
+        consumo_nuevo = Debitos(4, "USB")
+        billetera.Consumir(consumo_nuevo,"8")
         self.assertEqual(billetera.Saldo(),3,"No funciona consumir credito cuando se tiene suficiente")
 
+    def testRecargaConsumoMinimo(self):
+        billetera = BilleteraElectronica(12,'sergio','barrios',24101133, "8")
+        cred = Creditos(0.01 ,"USB")
+        deb = Debitos(0.01, "USB")
+        billetera.Recargar(cred)
+        billetera.Consumir(deb,"8")
+        self.assertEqual(billetera.Saldo(),0,"No funciona la operacion minima")    
+        
+    def testMegaRecargaMegaConsumo(self):
+        billetera = BilleteraElectronica(2,'francisco','sucre',19564959,"142")
+        deb = Debitos(2^31, "USB")
+        cred = Creditos((2^31) + 1, "USB")
+        billetera.Recargar(cred)
+        billetera.Consumir(deb,"142")
+        self.assertEqual(billetera.Saldo(), 1, "Existen Errores con la recarga/consumo grande")
+
+    def testRecargaNoPositiva(self):
+        billetera = BilleteraElectronica(3,'Stefani','Castellanos',25385981, "1023")
+        cred = Creditos(-1, "USB")
+        self.assertRaises(Exception, billetera.Recargar, cred)
+        
+    def testConsumoNopositivo(self):
+        billetera = BilleteraElectronica(3,'Stefani','Castellanos',25385981,"1023")
+        deb = Debitos(-1, "USB")
+        self.assertRaises(Exception, billetera.Consumir, deb)
+        
+    def testConsumoDecimalInvalido(self):
+        billetera = BilleteraElectronica(1,'sergio','barrios',24101133, "8")
+        deb = Debitos(2.01, "USB")
+        cred = Creditos(2, "USB")
+        billetera.Recargar(cred) 
+        self.assertRaises(Exception, billetera.Consumir,deb)
+        
+    def testEntradaStringCred(self):
+        billetera = BilleteraElectronica(1,'sergio','barrios',24101133, "8")
+        self.assertRaises(Exception, Creditos, "hola", "USB" )
+        
+    def testEntradaStringDeb(self):
+        billetera = BilleteraElectronica(1,'sergio','barrios',24101133, "8")
+        self.assertRaises(Exception, Debitos, "hola", "USB" )        
+        
+    def testEntradaBooleanoCreditos(self):
+        billetera = BilleteraElectronica(1,'sergio','barrios',24101133, "8")
+        self.assertRaises(Exception, Creditos, True, "USB" )
+        
+    def testEntradaBooleanoDebitos(self):
+        billetera = BilleteraElectronica(1,'sergio','barrios',24101133, "8")
+        self.assertRaises(Exception, Debitos, True,  "USB" )
+            
+    def testNombreCaracteresEspeciales(self):
+        billetera = BilleteraElectronica(1024,'Ram�n','Nu�ez',3981023, "8")
+        cred = Creditos(1,"USB")
+        billetera.Recargar(cred)
+        self.assertEqual(billetera.Saldo(), 1,"No funcionan los caracteres especiales")      
+        
+    def testCedulaNegativa(self):
+        self.assertRaises(Exception, BilleteraElectronica, 1,'Ram�n','Nu�ez',-237920, "8")
+    
+    def testFechaNoEsDateTime(self):
+        self.assertRaises(Exception, Creditos, 1,"kkkaajsjjddff1212j","USB")
+        
+    def testBilleteraStringVacio(self):
+        billetera = BilleteraElectronica(1024,'','',3981023, "")
+        cred = Creditos(1,"USB")
+        billetera.Recargar(cred)
+        self.assertEqual(billetera.Saldo(), 1,"Hay un error con entradas de string vacios")      
+ 
+    def testCreditoStringVacio(self):
+        billetera = BilleteraElectronica(1024,'Ram�n','Nu�ez',237920, "8")
+        cred = Creditos(1,"")
+        billetera.Recargar(cred)
+        self.assertEqual(billetera.Saldo(), 1,"Hay un error con entradas de string vacios")              
+      
+    def testDebitoStringVacio(self):
+        billetera = BilleteraElectronica(1024,'Ram�n','Nu�ez',237920, "8")
+        cred = Creditos(1,"")
+        billetera.Recargar(cred)        
+        deb = Debitos(1,"")
+        billetera.Consumir(deb,"8")
+        self.assertEqual(billetera.Saldo(), 0,"Hay un error con entradas de string vacios")          
         
 if __name__ == "__main__":
     unittest.main()
